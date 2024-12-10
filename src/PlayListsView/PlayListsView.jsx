6718e4c6bb11ccac7f9a2d34da573b15193c27b0
Adam Dasovich <adamdasovich@gmail.com>
2024-12-10 13:06:32 -0500

    playlists look ok now

--- src/PlayListsView/PlayListsView.jsx
+++ src/PlayListsView/PlayListsView.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import './PlayListsView.css'
 
 const PlayListsView = ({playlists, onBack}) => {
   console.log(playlists)
   return (
     <div className='PlayListsView'>
       <h2>Your Playlists</h2>
-      <button onClick={onBack}>Back to Search</button>
-      <ul>
+      <button
+        className='PlayListsView-onBack' 
+        onClick={onBack}
+      >Back to Search
+      </button>
+      <ul className='user-playlists'>
         {playlists.map(playlist => (
           <li key={playlist.id}>{playlist.name} - {playlist.trackCount} tracks</li>
         )
